205b57a952350797206f39e171e4f0bdcdc7c206
SthaRaunak <codewithraunak2006@gmail.com>
2024-05-29 23:54:27 +0545

    added header for add transformation page

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Header from "@/components/shared/Header";
+import { transformationTypes } from "../../../../../../constants";
 
-function AddTransformationTypePage() {
-  return <div>AddTransformationTypePage</div>;
+function AddTransformationTypePage({
+  params,
+}: {
+  params: {
+    type: "fill" | "restore" | "remove" | "removeBackground" | "recolor";
+  };
+}) {
+  const { type } = params;
+
+  const transformation = transformationTypes[type];
+
+  return (
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+    </>
+  );
 }
 
 export default AddTransformationTypePage;
